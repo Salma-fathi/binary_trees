3cd2327482b4c287e20160226a47004d3039a5ac
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-02-27 17:50:27 +0300

    tas3c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if(tree == NULL)
+    {
+        return(NULL);
+    }
+
+    binary_tree_delete(tree->left);
+
+    binary_tree_delete(tree->return);
+    
+    
+    free(tree);
+}
