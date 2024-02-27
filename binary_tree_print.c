543ec7839e451141cb2948a91ae10d1862b53783
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-02-27 19:07:19 +0300

    run5

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include "binary_trees.h"
 
-/* Original code from http://stackoverflow.com/a/13755911/5184480 */
-
 /**
  * print_t - Stores recursively each level in an array of strings
  *
@@ -22,12 +20,15 @@ static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
 
 	if (!tree)
 		return (0);
+
 	is_left = (tree->parent && tree->parent->left == tree);
 	width = sprintf(b, "(%03d)", tree->n);
 	left = print_t(tree->left, offset, depth + 1, s);
 	right = print_t(tree->right, offset + left + width, depth + 1, s);
+
 	for (i = 0; i < width; i++)
 		s[depth][offset + left + i] = b[i];
+
 	if (depth && is_left)
 	{
 		for (i = 0; i < width + right; i++)
@@ -40,6 +41,7 @@ static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
 			s[depth - 1][offset - width / 2 + i] = '-';
 		s[depth - 1][offset + left + width / 2] = '.';
 	}
+
 	return (left + width + right);
 }
 
@@ -57,6 +59,7 @@ static size_t _height(const binary_tree_t *tree)
 
 	height_l = tree->left ? 1 + _height(tree->left) : 0;
 	height_r = tree->right ? 1 + _height(tree->right) : 0;
+
 	return (height_l > height_r ? height_l : height_r);
 }
 
@@ -72,10 +75,12 @@ void binary_tree_print(const binary_tree_t *tree)
 
 	if (!tree)
 		return;
+
 	height = _height(tree);
 	s = malloc(sizeof(*s) * (height + 1));
 	if (!s)
 		return;
+
 	for (i = 0; i < height + 1; i++)
 	{
 		s[i] = malloc(sizeof(**s) * 255);
@@ -83,7 +88,9 @@ void binary_tree_print(const binary_tree_t *tree)
 			return;
 		memset(s[i], 32, 255);
 	}
+
 	print_t(tree, 0, 0, s);
+
 	for (i = 0; i < height + 1; i++)
 	{
 		for (j = 254; j > 1; --j)
@@ -95,5 +102,6 @@ void binary_tree_print(const binary_tree_t *tree)
 		printf("%s\n", s[i]);
 		free(s[i]);
 	}
+
 	free(s);
 }
