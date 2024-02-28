cbc88c04c6357e82148f61f82d0b6645f587ee51
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-02-29 02:50:46 +0300

    bettyy

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+*
+*
+*/
+ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if(tree == NULL || func == NULL)
+        return NULL;
+
+    
+}
