3eded2dadaa8b2ddc6232214e21eb07294c4e18e
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-02-29 04:15:52 +0300

    betty101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -6,21 +6,21 @@
  *
  * Return: the height of the tree. If tree is NULL, return 0
  */
-binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height_l;
-    size_t height_r;
+	size_t height_l;
+	size_t height_r;
+	
+	if (tree == NULL)
+		return (0);
 
-    if (tree == NULL)
-        return (0);
+	height_l = binary_tree_height(tree->left);
+	height_r = binary_tree_height(tree->right);
 
-    height_l = binary_tree_height(tree->left);
-    height_r = binary_tree_height(tree->right);
-
-    if (height_l >= height_r)
-        return (height_l + 1);
-    else
-        return (height_r + 1);
+	if (height_l >= height_r)
+		return (height_l + 1);
+	else
+		return (height_r + 1);
 }
 
 /**
@@ -33,19 +33,19 @@ binary_tree_height(const binary_tree_t *tree)
  */
 void binary_tree_level(const binary_tree_t *tree, size_t l, void (*func)(int))
 {
-    if(tree == NULL)
-    return NULL;
+	if (tree == NULL)
+		return;
 
-    if (l == 1)
-    {
-        func(tree->n);
-        return;
-    }
-    else if (l > 1)
-    {
-        binary_tree_level(tree->left, l - 1, func);
-        binary_tree_level(tree->right, l - 1, func);
-    }
+	if (l == 1)
+	{
+		func(tree->n);
+		return;
+	}
+	else if (l > 1)
+	{
+		binary_tree_level(tree->left, l - 1, func);
+		binary_tree_level(tree->right, l - 1, func);
+	}
 }
 
 /**
@@ -56,14 +56,14 @@ void binary_tree_level(const binary_tree_t *tree, size_t l, void (*func)(int))
  *
  * Return: void
  */
- void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    size_t height ,s;
-    if(tree == NULL || func == NULL)
-        return NULL;
-    
-    height = binary_tree_height(tree);
-    for (s = 1; s <= height; s++)
-        binary_tree_level(tree, s, func);
+	size_t height, s;
+	
+	if (tree == NULL || func == NULL)
+		return;
 
+	height = binary_tree_height(tree);
+	for (s = 1; s <= height; s++)
+		binary_tree_level(tree, s, func);
 }
