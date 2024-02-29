52dcd5f6f68fef2f5fdd40fd79d1468a07e5ad74
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-02-29 03:08:09 +0300

    khkh

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,22 +7,23 @@
  * Return: pointer to the lowest common ancestor node of the two given nodes
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                     const binary_tree_t *second)
+const binary_tree_t *second)
 {
-    binary_tree_t *tmp;
-    if (first == NULL || second == NULL)
-        return (NULL);
-    tmp = (binary_tree_t *)second;
-    while (first)
-    {
-        while (second)
-        {
-            if (first == second)
-                return ((binary_tree_t *)first);
-            second = second->parent;
-        }
-        second = tmp;
-        first = first->parent;
-    }
-    return (NULL);
+	binary_tree_t *tmp;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+	tmp = (binary_tree_t *)second;
+	while (first)
+	{
+		while (second)
+		{
+			if (first == second)
+				return ((binary_tree_t *)first);
+			second = second->parent;
+		}
+		second = tmp;
+		first = first->parent;
+	}
+	return (NULL);
 }
