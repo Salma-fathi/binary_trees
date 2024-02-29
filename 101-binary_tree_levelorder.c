a28ab8d93a0473b2f1c655a40c60fcbf3f31ae33
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-02-29 04:11:52 +0300

    tas101sk

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,13 +1,69 @@
 #include "binary_trees.h"
 
 /**
-*
-*
-*/
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height of
+ *
+ * Return: the height of the tree. If tree is NULL, return 0
+ */
+binary_tree_height(const binary_tree_t *tree)
+{
+    size_t height_l;
+    size_t height_r;
+
+    if (tree == NULL)
+        return (0);
+
+    height_l = binary_tree_height(tree->left);
+    height_r = binary_tree_height(tree->right);
+
+    if (height_l >= height_r)
+        return (height_l + 1);
+    else
+        return (height_r + 1);
+}
+
+/**
+ * binary_tree_level - perform a function on a specific level of a binary tree
+ * @tree: pointer to the root of the tree
+ * @l: level of the tree to perform a function on
+ * @func: function to perform
+ *
+ * Return: void
+ */
+void binary_tree_level(const binary_tree_t *tree, size_t l, void (*func)(int))
+{
+    if(tree == NULL)
+    return NULL;
+
+    if (l == 1)
+    {
+        func(tree->n);
+        return;
+    }
+    else if (l > 1)
+    {
+        binary_tree_level(tree->left, l - 1, func);
+        binary_tree_level(tree->right, l - 1, func);
+    }
+}
+
+/**
+ * binary_tree_levelorder - traverses a binary tree using level-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter to this function
+ *
+ * Return: void
+ */
  void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
+    size_t height ,s;
     if(tree == NULL || func == NULL)
         return NULL;
-
     
+    height = binary_tree_height(tree);
+    for (s = 1; s <= height; s++)
+        binary_tree_level(tree, s, func);
+
 }
