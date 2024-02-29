59d802187aad37c0051e7a5e1f3110173610284c
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-02-29 16:47:07 +0300

    miss line

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,6 +8,7 @@
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *new_root;
+    
 	if (tree == NULL || tree->right == NULL)
 		return (NULL);
 	new_root = tree->right;
